5a45b5e623caead1983d17363405d888892f88fb
Dalton Cézane <daltoncezane@gmail.com>
2018-09-06 19:43:13 -0300

    A few lines to generate an RSA key pair in the CA.

--- host/main.c
+++ host/main.c
@@ -1,6 +1,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+#include <openssl/rsa.h>
 
 /* OP-TEE TEE client API (built by optee_client) */
 #include <tee_client_api.h>
@@ -10,6 +11,7 @@
 #define RSA_KEY_SIZE 1024
 #define RSA_MAX_PLAIN_LEN_1024 86 // (1024/8) - 42 (padding)
 #define RSA_CIPHER_LEN_1024 (RSA_KEY_SIZE / 8)
+#define RSA_EXP 17 //generally 3, 17 or 65537
 
 struct ta_attrs {
 	TEEC_Context ctx;
@@ -50,6 +52,47 @@ void prepare_op(TEEC_Operation *op, char *in, size_t in_sz, char *out, size_t ou
 	op->params[0].tmpref.size = in_sz;
 	op->params[1].tmpref.buffer = out;
 	op->params[1].tmpref.size = out_sz;
+}
+
+void rsa_gen_keys_ca() {
+	RSA *keypair = NULL;
+	unsigned char *pub_key = NULL;
+	BIGNUM *bne = NULL;
+	unsigned long e = RSA_F4;
+	int success = 0;
+
+	bne = BN_new();
+    success = BN_set_word(bne, e);
+    if (!success) {
+		errx(1, "\nrsa_gen_keys_ca failed at BN_set_word result.");
+        goto free_all;
+    }
+
+	keypair = RSA_new();
+	success = RSA_generate_key_ex(keypair, RSA_KEY_SIZE, bne, NULL);
+    if (!success) {
+		errx(1, "\nrsa_gen_keys_ca failed at RSA_generate_key_ex result.");
+        goto free_all;
+    }
+
+	success = i2d_RSAPublicKey(keypair, &pub_key);
+	if (success < 0) {
+		errx(1, "\nrsa_gen_keys_ca failed at i2d_RSAPublicKey result.");
+		goto free_all;
+	}
+	printf("==========RSA Public Key successfuly extracted: %s", pub_key);
+	// BIO *priv_key = BIO_new(BIO_s_mem());
+	// BIO *pub_key = BIO_new(BIO_s_mem());
+	// PEM_write_bio_RSAPrivateKey(priv_key, keypair, NULL, NULL, 0, NULL, NULL);
+	// PEM_write_bio_RSAPublicKey(pub_key, keypair);
+
+
+	//TODO get the pub and priv parts in bytes format...
+free_all:
+	// BIO_free_all(pub_key);
+    // BIO_free_all(priv_key);
+    RSA_free(keypair);
+    BN_free(bne);
 }
 
 void rsa_gen_keys(struct ta_attrs *ta) {
@@ -95,17 +138,20 @@ void rsa_decrypt(struct ta_attrs *ta, char *in, size_t in_sz, char *out, size_t 
 int main(int argc, char *argv[])
 {//TODO add a good print function (fflush)
 	struct ta_attrs ta;
-	char clear[RSA_MAX_PLAIN_LEN_1024];
-	char ciph[RSA_CIPHER_LEN_1024];
+	// char clear[RSA_MAX_PLAIN_LEN_1024];
+	// char ciph[RSA_CIPHER_LEN_1024];
+	rsa_gen_keys_ca();
 	
 	prepare_ta_session(&ta);
-	printf("\nType something to be encrypted and decrypted in the TA:\n");
-	fflush(stdin); //setbuf(stdin, NULL);
-	fgets(clear, sizeof(clear), stdin);
+	//printf("\nType something to be encrypted and decrypted in the TA:\n");
+	//fflush(stdin); //setbuf(stdin, NULL);
+	//fgets(clear, sizeof(clear), stdin);
 
 	rsa_gen_keys(&ta);
-	rsa_encrypt(&ta, clear, RSA_MAX_PLAIN_LEN_1024, ciph, RSA_CIPHER_LEN_1024);
-	rsa_decrypt(&ta, ciph, RSA_CIPHER_LEN_1024, clear, RSA_MAX_PLAIN_LEN_1024);
+	//rsa_encrypt(&ta, clear, RSA_MAX_PLAIN_LEN_1024, ciph, RSA_CIPHER_LEN_1024);
+	//rsa_decrypt(&ta, ciph, RSA_CIPHER_LEN_1024, clear, RSA_MAX_PLAIN_LEN_1024);
+
+	//
 	
 	terminate_tee_session(&ta);
 	return 0;
